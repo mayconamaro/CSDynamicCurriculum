8e223df1769f62daebc1f44a353d5d6d56c0a479
Flavia Nunes <flavianunes@decom.ufop.br>
2020-02-18 18:48:54 -0300

    fixes json parse

--- curriculum.js
+++ curriculum.js
@@ -1,9 +1,40 @@
 // parse json file
-const courses = require('./courses.json');
+var courses;
+function loadJSON(callback) {
+    //criamos uma instancia da xmlhttprequest
+    const xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'courses.json', false);
+    //carregamos o cnoteudo do courses.json de forma sync (se colocar true ali em cima, muda pra async)
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") { //tivemos sucesso nesse get?
+            //siiiim
+            callback(xobj.responseText);
+            //chama a funcao q passamos por callback :)
+        }
+    };
+    ///naaaao :( mandamos nulll
+    xobj.send(null);
+}
+
+loadJSON( response => {
+    //funcao q passamos pro callback vai fazer oq, fazer um parse na resposta da request e armazenar isso numa variavel global :)
+    courses = JSON.parse(response);
+
+});
+
+console.log(courses);
 
 
 //create element
-const createCourseElement = ({ id, title, credits, requires, quarter, isCompleted }) => {
+const createCourseElement = ({
+    id,
+    title,
+    credits,
+    requires,
+    quarter,
+    isCompleted
+}) => {
 
     const course = document.createElement('div');
     const courseTitle = document.createElement('h2');
@@ -11,7 +42,7 @@ const createCourseElement = ({ id, title, credits, requires, quarter, isComplete
 
     course.id = id;
     course.className = 'course';
-    course.onclick = function EventHandler () {
+    course.onclick = function EventHandler() {
         //change course color
         //add course credits to total credits if isCompleted == false
         //remove course credits from total credits if isCompleted == true
@@ -33,15 +64,14 @@ const completeCourse = (course) => {
 
     const courseData = courses.find(c => course.id == c.id);
 
-    if(!courseData.isCompleted){
+    if (!courseData.isCompleted) {
 
-        const isRequirementsMet = courseData.requires.map( c => c.isCompleted );
-        
-        if(isRequirementsMet.every( c => c )){
+        const isRequirementsMet = courseData.requires.map(c => c.isCompleted);
+
+        if (isRequirementsMet.every(c => c)) {
             console.log("requirements met!");
         } else {
             console.log("some requirements aren't met");
         }
     }
 }
-
